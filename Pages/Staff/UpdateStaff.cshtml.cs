c3b19f5c0ec49f2d0c459a5e33c376e9b8d315e5
Aahmad <ch2shahid@gmail.com>
2021-04-18 06:19:30 -0600

    Added Update functionality for Staff

--- Pages/Staff/UpdateStaff.cshtml.cs
+++ Pages/Staff/UpdateStaff.cshtml.cs
@@ -1,16 +1,44 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResolutionsPsych.Classes;
 
 namespace ResolutionsPsych.Pages
 {
+    [BindProperties(SupportsGet =true)]
     public class UpdateStaffModel : PageModel
     {
+        public Login ToUpdateStaff {get;set;}
+
+        [Required(ErrorMessage = "name is required"), RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "name is invalid"),
+           MaxLength(10, ErrorMessage = "name is too long")]
+        public string NewUsername { get; set; }
+        [Required, DataType(DataType.Password), MinLength(3), MaxLength(10)]
+        public string NewPassword { get; set; }
+        public string Message { get; set; }
         public void OnGet()
         {
+            ResolutionsSystem rs = new ResolutionsSystem();
+            
+            ToUpdateStaff = rs.GetLogin(HttpContext.Session.GetString("ToModifyStaffID"));
+            
+        }
+        public IActionResult OnPost()
+        {
+            ResolutionsSystem rs = new ResolutionsSystem();
+
+            Login UpdatedLogin = new Login();
+            UpdatedLogin.Username = NewUsername;
+            UpdatedLogin.Password = NewPassword;
+
+            rs.UpdateLogin(UpdatedLogin);
+
+            return Page();
         }
     }
 }
